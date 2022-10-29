f334fc198b85316ea18b716336bdc5ae31bc162e
Ngufor-emmanuella <emmanuellangufor@gmail.com>
2022-10-29 15:56:53 +0100

    Replaced then with await

--- src/modules/createtr.js
+++ src/modules/createtr.js
@@ -1,28 +1,24 @@
 const renderContainer = async (gameLInk) => {
   const res = await fetch(gameLInk);
-  await res.json()
-    .then((resData) => {
-      const container = document.querySelector('.table');
-      container.replaceChildren();
-      let counts = 1;
-      resData.result.forEach((scores, index) => {
-        container.innerHTML += `
+  const resData = await res.json();
+  const container = document.querySelector('.list');
+  container.replaceChildren();
+  let counts = 1;
+  resData.result.forEach((scores, index) => {
+    container.innerHTML += `
         <li>
           <p>${scores.user}:</p>
           <p>${scores.score}</p>
         </li> `;
-        container.classList.add('x');
-        if (counts % 2 === 0) {
-          container.children[index].classList.add('color');
-        } else {
-          container.children[index].classList.add('no-color');
-        }
-
-        counts += 1;
-      });
-    });
+    container.classList.add('x');
+    if (counts % 2 === 0) {
+      container.children[index].classList.add('color');
+    } else {
+      container.children[index].classList.add('no-color');
+    }
+    counts += 1;
+  });
 };
-
 const toAddScore = async (gameLInk, user, score) => {
   await fetch(gameLInk, {
     method: 'POST',
@@ -35,5 +31,4 @@ const toAddScore = async (gameLInk, user, score) => {
     }),
   });
 };
-
 export { renderContainer, toAddScore };
