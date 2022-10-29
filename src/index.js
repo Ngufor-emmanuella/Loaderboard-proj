fe3c64b3e21b4d14548d5f83e3035cedda379d9b
Ngufor-emmanuella <emmanuellangufor@gmail.com>
2022-10-29 13:29:01 +0100

    removed commented codes

--- src/index.js
+++ src/index.js
@@ -1,29 +1,7 @@
 // eslint-disable-next-line
-// import _ from 'lodash';
-// import inputTable from './modules/inputtable.js';
-// import tableValues from './modules/tableobj.js';
 import { renderContainer, toAddScore } from './modules/createtr.js';
 
 import './style.css';
-
-// inputTable(tableValues);
-
-// let gameIdentity = '';
-// fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games', {
-
-//   method: 'POST',
-//   headers: {
-//     'Content-type': 'application/json; charset=UTF-8',
-//   },
-//   body: JSON.stringify({
-//     name: 'Rocking ball',
-//   }),
-// }).then((res) => res.json())
-//   .then((gameID) => {
-//     const response = gameID.result;
-//     gameIdentity = response.substring(14, response.lastIndexOf(' '));
-//     console.log(gameIdentity);
-//   });
 
 const refresh = document.querySelector('#refresh-button');
 
